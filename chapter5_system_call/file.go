ae0fb5249fa94081601452c26a22a2b6254cadc6
timtoronto634 <tim225x@gmail.com>
2023-04-15 17:41:49 +0900

    add notes for system call code reading

--- chapter5_system_call/file.go
+++ chapter5_system_call/file.go
@@ -9,6 +9,7 @@ func CreateFile(filename string) {
 	if !strings.HasSuffix(filename, ".txt") {
 		filename = filename + ".txt"
 	}
+	// Create call will reach system call defined in C at: /go/src/runtime/internal/syscall/asm_linux_amd64.s
 	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
