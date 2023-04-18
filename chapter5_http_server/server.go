947b78ade8b8d5ce19f6492fbb55d19c155c98d8
timtoronto634 <tim225x@gmail.com>
2023-04-18 20:00:41 +0900

    6.5.1 server with tcp socket

--- chapter5_http_server/server.go
+++ chapter5_http_server/server.go
@@ -1,28 +1,47 @@
 package chapter5httpserver
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
 )
 
 func Serve() error {
-	ln, err := net.Listen("tcp", ":8887")
+	// open socket
+	listener, err := net.Listen("tcp", ":8887")
 	if err != nil {
 		panic(err)
 	}
 	for {
-		conn, err := ln.Accept()
+		// tcp connection
+		conn, err := listener.Accept()
 		if err != nil {
 			panic(err)
 		}
 		go func() {
-			var b []byte
-			_, err = conn.Read(b)
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			request, err := http.ReadRequest(
+				bufio.NewReader(conn))
 			if err != nil {
 				panic(err)
 			}
-
-			fmt.Println(b)
+			dump, err := httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(dump))
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body:       io.NopCloser(strings.NewReader("Hello World\n")),
+			}
+			response.Write(conn)
+			conn.Close()
 		}()
 	}
 }
