0e38aa89fc3140b103905b84d6bdb981436649b3
timtoronto634 <tim225x@gmail.com>
2023-04-26 00:18:10 +0900

    Fix on
    https://github.com/yurakawa/learn-system-programming-with-go/blob/master/ch6/s9-1/server.go

--- chapter6_http_server/client.go
+++ chapter6_http_server/client.go
@@ -15,24 +15,22 @@ func ClientDo() {
 		"PROGRAMMING",
 		"PLUS",
 	}
+	current := 0
 	var conn net.Conn = nil
 	var err error
-	requests := make([]*http.Request, 0, len(sendMessages))
+	requests := make(chan *http.Request, len(sendMessages))
 
 	conn, err = net.Dial("tcp", "localhost:8887")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Access\n")
+	fmt.Printf("Access: %d\n", current)
 	defer conn.Close()
 
+	// リクエストだけ先に送る
 	for i := 0; i < len(sendMessages); i++ {
 		lastMessage := i == len(sendMessages)-1
-		request, err := http.NewRequest(
-			"GET",
-			"http://localhost:8887?message="+sendMessages[i],
-			nil,
-		)
+		request, err := http.NewRequest("GET", "http://localhost:8887?message="+sendMessages[i], nil)
 		if lastMessage {
 			request.Header.Add("Connection", "close")
 		} else {
@@ -46,19 +44,24 @@ func ClientDo() {
 			panic(err)
 		}
 		fmt.Println("send: ", sendMessages[i])
-		requests = append(requests, request)
+		requests <- request
 	}
+	close(requests)
 
+	// レスポンスをまとめて受信
 	reader := bufio.NewReader(conn)
-	for _, request := range requests {
+	for request := range requests {
 		response, err := http.ReadResponse(reader, request)
 		if err != nil {
 			panic(err)
 		}
-		dump, err := httputil.DumpResponse(response, false)
+		dump, err := httputil.DumpResponse(response, true)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(dump))
+		if current == len(sendMessages) {
+			break
+		}
 	}
 }
