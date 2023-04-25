b84fc3f844829e6ac3198ea10ba28904896a5cdb
timtoronto634 <tim225x@gmail.com>
2023-04-26 00:05:19 +0900

    6.9.2 client

--- chapter6_http_server/client.go
+++ chapter6_http_server/client.go
@@ -2,14 +2,10 @@ package chapter5httpserver
 
 import (
 	"bufio"
-	"compress/gzip"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
-	"os"
-	"strings"
 )
 
 // dial localhost:8080
@@ -19,64 +15,50 @@ func ClientDo() {
 		"PROGRAMMING",
 		"PLUS",
 	}
-	current := 0
 	var conn net.Conn = nil
+	var err error
+	requests := make([]*http.Request, 0, len(sendMessages))
 
-	for {
-		var err error
-		if conn == nil {
-			conn, err = net.Dial("tcp", "localhost:8887")
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Access: %d\n", current)
-		}
+	conn, err = net.Dial("tcp", "localhost:8887")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Access\n")
+	defer conn.Close()
+
+	for i := 0; i < len(sendMessages); i++ {
+		lastMessage := i == len(sendMessages)-1
 		request, err := http.NewRequest(
-			"POST",
-			"http://localhost:8887",
-			strings.NewReader(sendMessages[current]),
+			"GET",
+			"http://localhost:8887?message="+sendMessages[i],
+			nil,
 		)
+		if lastMessage {
+			request.Header.Add("Connection", "close")
+		} else {
+			request.Header.Add("Connection", "keep-alive")
+		}
 		if err != nil {
 			panic(err)
 		}
-		request.Header.Add("Accept-Encoding", "gzip") // NOTE: Set will replace the existing value
-
 		err = request.Write(conn)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println("send: ", sendMessages[i])
+		requests = append(requests, request)
+	}
 
-		response, err := http.ReadResponse(
-			bufio.NewReader(conn), request)
+	reader := bufio.NewReader(conn)
+	for _, request := range requests {
+		response, err := http.ReadResponse(reader, request)
 		if err != nil {
-			fmt.Println("Retry")
-			conn = nil
-			continue
+			panic(err)
 		}
-		defer response.Body.Close()
-
-		// only for showing to standard output
 		dump, err := httputil.DumpResponse(response, false)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(dump))
-
-		if response.Header.Get("Content-Encoding") == "gzip" {
-			gzipReader, err := gzip.NewReader(response.Body)
-			if err != nil {
-				panic(err)
-			}
-			io.Copy(os.Stdout, gzipReader)
-			gzipReader.Close()
-		} else {
-			io.Copy(os.Stdout, response.Body)
-		}
-
-		current++
-
-		if current == len(sendMessages) {
-			break
-		}
 	}
 }
