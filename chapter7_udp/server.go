f3a0c509e538de758367ee9c8c976f72131c9fb9
timtoronto634 <tim225x@gmail.com>
2023-04-30 20:40:18 +0900

    7.2.1 server

---
+++ chapter7_udp/server.go
@@ -0,0 +1,30 @@
+package chapter5httpserver
+
+import (
+	"fmt"
+	"net"
+)
+
+func Serve() error {
+	port := ":8887"
+	// open socket
+	conn, err := net.ListenPacket("udp", "localhost:8887")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("server is running at port%s\n", port)
+	defer conn.Close()
+
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Received from %s: %v\n", remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"), remoteAddress)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
