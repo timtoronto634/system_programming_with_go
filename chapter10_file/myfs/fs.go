9df009496ab8c8643cc0603d689806957231ac49
timtoronto634 <tim225x@gmail.com>
2023-05-06 15:35:30 +0900

    Feat add getattr

--- chapter10_file/myfs/fs.go
+++ chapter10_file/myfs/fs.go
@@ -1,6 +1,9 @@
 package myfs
 
 import (
+	"context"
+	"strings"
+
 	"github.com/winfsp/cgofuse/fuse"
 	"gocloud.dev/blob"
 )
@@ -8,4 +11,27 @@ import (
 type CloudFileSystem struct {
 	fuse.FileSystemBase
 	bucket *blob.Bucket
+}
+
+func (cf *CloudFileSystem) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
+	if path == "/" {
+		stat.Mode = fuse.S_IFDIR | 0555
+		return 0
+	}
+	ctx := context.Background()
+	name := strings.TrimLeft(path, "/")
+	a, err := cf.bucket.Attributes(ctx, name)
+	if err != nil {
+		_, err := cf.bucket.Attributes(ctx, name+"/")
+		if err != nil {
+			return -fuse.ENOENT
+		}
+		stat.Mode = fuse.S_IFDIR | 0555
+	} else {
+		stat.Mode = fuse.S_IFREG | 0444
+		stat.Size = a.Size
+		stat.Mtim = fuse.NewTimespec(a.ModTime)
+	}
+	stat.Nlink = 1
+	return 0
 }
